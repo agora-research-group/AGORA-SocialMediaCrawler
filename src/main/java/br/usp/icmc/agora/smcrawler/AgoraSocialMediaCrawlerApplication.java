8a1c5f4bf80503018e30a9c2cceb3ad398eb4b18
Flavio Horita <feahorita@gmail.com>
2016-08-03 17:42:47 -0300

    Initial commit

---
+++ src/main/java/br/usp/icmc/agora/smcrawler/AgoraSocialMediaCrawlerApplication.java
@@ -0,0 +1,12 @@
+package br.usp.icmc.agora.smcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgoraSocialMediaCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgoraSocialMediaCrawlerApplication.class, args);
+	}
+}
---
+++ src/test/java/br/usp/icmc/agora/smcrawler/AgoraSocialMediaCrawlerApplicationTests.java
@@ -0,0 +1,16 @@
+package br.usp.icmc.agora.smcrawler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AgoraSocialMediaCrawlerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
